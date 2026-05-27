51b8f6c9fa1fe44ad1d304a935d221921f900fb6
TalesCampos23 <talescampoos15@gmail.com>
2026-05-27 11:33:34 -0300

    updatemap0.1

--- new-game-project/new-game-project/Scripts/player.gd
+++ new-game-project/new-game-project/Scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
+const JUMP_VELOCITY = -550.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
